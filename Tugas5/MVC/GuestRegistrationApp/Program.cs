a792b88d34efd27f0fc93ef8cb9388d79e5c39e6
AfaldoDA <aldodanny3@gmail.com>
2025-07-08 05:09:36 +0700

    Update Program.cs

--- Tugas5/MVC/GuestRegistrationApp/Program.cs
+++ Tugas5/MVC/GuestRegistrationApp/Program.cs
@@ -1,22 +1,15 @@
-using GuestRegistrationApp.Models;
-using MySql.Data.MySqlClient;
-using System.Data;
+using System;
+using System.Windows.Forms;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Konfigurasi Dapper
-builder.Services.AddScoped<IDbConnection>(_ => 
-    new MySqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<DatabaseContext>();
-
-var app = builder.Build();
-app.Urls.Add("http://localhost:5000");
-app.Urls.Add("https://localhost:5001");
-
-// ... middleware lainnya ...
-
-app.Run();
+namespace GuestRegistrationApp
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize(); // untuk .NET 6 ke atas
+            Application.Run(new GuestRegistrationApp.Views.MainForm()); // panggil namespace lengkap langsung
+        }
+    }
+}
